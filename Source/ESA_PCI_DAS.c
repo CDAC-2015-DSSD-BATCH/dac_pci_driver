2b9013d687bfda2b95713cfc9781480812c3f269
ShreyasHemachandran <shreyas.hemachandran@gmail.com>
2015-07-22 22:25:19 +0530

    Iteration 2 - Registration, Probing, Enabling PCI Device

--- Source/ESA_PCI_DAS.c
+++ Source/ESA_PCI_DAS.c
@@ -19,14 +19,40 @@
 #include<linux/module.h>
 #include<linux/pci.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Shreyas Hemachandran, Aldonraj, Asheerudin Khilji, Deepak Kumar");
-MODULE_VERSION("0.0");
+#define VENDOR_ID 0x10b5 
+#define DEVICE_ID 0x9050
+#define SUBVENDOR_ID 0x4441
+#define SUBDEVICE_ID 0x4144
+
+#define BASE_ADDR 0x0000 // TODO Base Address yet to be discovered
+#define MASTER_REG (BASE_ADDR+E)
+#define LD_CL_CHANL (BASE_ADDR+8)
+#define ADC_MODE_REG (BASE_ADDR+9)
+#define ADC_STATUS_REG (BASE_ADDR+9)
+#define LOWER_DByte (BASE_ADDR+A)
+#define HIGHER_DByte (BASE_ADDR+B)
+#define POLLINIT_REG (BASE_ADDR+D)
+
+// Prototypes
+int DAS_probe (struct pci_dev *dev, const struct pci_device_id *id);
+void DAS_remove (struct pci_dev *dev); 
+
+// Globals Variables
+struct pci_device_id DAS_id={
+	.vendor=VENDOR_ID,
+	.device=DEVICE_ID,
+	.subvendor=SUBVENDOR_ID,
+	.subdevice=SUBDEVICE_ID,
+};
 
 static struct pci_driver pci_driver={
 	.name="ESA_PCI_DSA",
+	.id_table=&DAS_id,
+	.probe=DAS_probe,
+	.remove=DAS_remove,
 };
 
+// Functions
 static int __init DAS_init(void){
 	printk(KERN_ALERT "%s initialized!!",pci_driver.name);
 	return pci_register_driver(&pci_driver);
@@ -37,5 +63,29 @@ static void __exit DAS_exit(void){
 	pci_unregister_driver(&pci_driver);
 }
 
-module_init(DAS_init);
+int DAS_probe(struct pci_dev *dev, const struct pci_device_id *id){
+	printk(KERN_ALERT "Vendor %x | Device %x -- Probing !",dev->vendor,dev->device);
+
+	if(pci_enable_device(dev)==0){
+		unsigned long BAR=0;
+		printk(KERN_ALERT "Device Enabled!!");
+
+		BAR=pci_resource_start(dev,2);
+		printk(KERN_ALERT "%p - Base Address",BAR);
+	}else{
+		printk(KERN_ALERT "Device Not Enabled!!");
+	}
+	return 0;
+}
+
+void DAS_remove(struct pci_dev *dev){
+	printk(KERN_ALERT "%s -- Removing!",pci_driver.name);
+}
+
+
 module_exit(DAS_exit);
+module_init(DAS_init);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shreyas Hemachandran, Aldonraj, Asheerudin Khilji, Deepak Kumar");
+MODULE_VERSION("0.0");
